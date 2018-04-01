c14f33c762cb835333b6cf0e9f17c4b9c772dd20
Ivan Vermeyen <ivan@vermeyen.org>
2018-04-01 11:32:22 +0200

    Make CombinedFilterTest more obvious

--- tests/Filters/CombinedFilterTest.php
+++ tests/Filters/CombinedFilterTest.php
@@ -11,11 +11,11 @@ class CombinedFilterTest extends TestCase
     /** @test */
     public function it_returns_a_combined_array_of_locales_and_languages()
     {
-        $browser = new BrowserLocale('en-US,nl-NL;q=0.8,nl;q=0.6');
+        $browser = new BrowserLocale('en-US,nl;q=0.8');
 
         $locales = $browser->filter(new CombinedFilter);
 
-        $this->assertEquals(['en-US', 'en', 'nl-NL', 'nl'], $locales);
+        $this->assertEquals(['en-US', 'en', 'nl'], $locales);
     }
 
     /** @test */
